896048eb33b5964b087e75c72f48fe1484c83b0d
Bolaji Oluwatofunmi Ashraf <tofunmibolaji13@gmail.com>
2023-12-20 19:48:09 +0100

    api

--- routes/root.js
+++ routes/root.js
@@ -2,8 +2,13 @@ const express = require("express")
 const router = express.Router()
 const path = require('path')
 
-router.get('^/$|/index(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'index.html'))
+
+router.get('/', async(req, res) => {
+    try {
+        res.sendFile(path.join(__dirname, '..', 'views', 'index.html'))
+    } catch (e) {
+        res.status(400).send(e.message);
+    }
 })
 
 module.exports = router
